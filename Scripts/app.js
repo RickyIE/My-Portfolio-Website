184dff41df27057cf4396ef38d1028d2de442642
Aleksandar <aleksandar.mladenov@student.griffith.ie>
2020-09-10 00:57:38 +0100

    Initial commit on my portfolio website build 1.0 on 10/09/2020

---
+++ Scripts/app.js
@@ -0,0 +1,108 @@
+
+
+// -----------------------Index Code -----------------------------//
+
+
+const button = document.querySelector(".MenuButton");
+const menuList = document.querySelector(".MenuList");
+const menuListBackground = document.querySelector(".MenuListBackground");
+const footerSection2ON = document.querySelector(".FooterSection2");
+footerSection2ON.addEventListener('mouseover', footerSection2AnimationON);
+const footerSection2OFF = document.querySelector(".FooterSection2");
+footerSection2OFF.addEventListener('mouseout', footerSection2AnimationOff);
+
+
+
+let pageTracker=1;
+let currentPage = document.getElementById("Slide");
+
+
+
+button.addEventListener('click', navBarControl);
+button.addEventListener('mouseover', navBarLightsOn);
+button.addEventListener('mouseout', navBarLightsOff);
+
+
+
+function navBarControl () {
+    if (menuList.style.display === 'none'){
+        menuList.style.display = 'grid'
+
+    } else {
+        menuList.style.display = 'none'
+    }
+
+
+}
+
+
+function navBarLightsOn () {
+
+    document.querySelector(".NavBarBottom").style.boxShadow ='5px 5px 10px lightcyan';
+    document.querySelector(".NavBarTop").style.boxShadow ='5px 5px 100px 10px lightcyan';
+
+}
+
+function navBarLightsOff () {
+
+
+    document.querySelector(".NavBarBottom").style.boxShadow ='none';
+    document.querySelector(".NavBarTop").style.boxShadow ='none';
+
+}
+
+let leftButton = document.getElementById("LeftButton").addEventListener('click', carouselMovementBackward);;
+let rightButton = document.getElementById("RightButton").addEventListener('click', carouselMovementForward);;
+let count=1;
+
+
+function carouselMovementForward () {
+    alert("Coming Soon to a screen near you !")
+    // currentPage.style.right = '500px';
+    // pageTracker++;
+    // currentPage = document.getElementById("Slide"+pageTracker);
+    // if (pageTracker === 4) {
+    //     pageTracker = 1;
+    //     currentPage = document.getElementById("Slide"+pageTracker);
+    //
+    // }
+
+}
+
+
+function carouselMovementBackward () {
+    alert("Coming Soon to a screen near you !")
+    // alert(pageTracker);
+    // alert(currentPage.innerText);
+
+}
+
+function footerSection2AnimationON () {
+
+    document.querySelector(".SocialContentLoadBarTop").style.width = "100%";
+    document.querySelector(".SocialContentLoadBarBottom").style.width = "100%";
+    document.querySelector(".SocialContentLoadBarTop").style.boxShadow = "0px 0px 5px 2px dodgerblue";
+    document.querySelector(".SocialContentLoadBarBottom").style.boxShadow = "0px 0px 5px 2px dodgerblue";
+    document.querySelector(".FooterSection1").style.filter = "blur(2px)";
+    document.querySelector(".FooterSection3").style.filter = "blur(2px)";
+
+    if (screen.width > 768) {
+        document.querySelector(".FooterSection2").style.transform = "Scale(1.200)";
+    }
+
+}
+
+
+function footerSection2AnimationOff () {
+
+    document.querySelector(".SocialContentLoadBarTop").style.width = "0%";
+    document.querySelector(".SocialContentLoadBarBottom").style.width = "0%";
+    document.querySelector(".SocialContentLoadBarTop").style.boxShadow = "0px 0px 0px 0px dodgerblue";
+    document.querySelector(".SocialContentLoadBarBottom").style.boxShadow = "0px 0px 0px 0px dodgerblue";
+    document.querySelector(".FooterSection1").style.filter = "none";
+    document.querySelector(".FooterSection3").style.filter = "none";
+    document.querySelector(".FooterSection2").style.transform = "Scale(1.000)";
+}
+
+
+
---
+++ Scripts/cardFlip.js
@@ -0,0 +1,39 @@
+//
+//
+// let card = document.querySelector(".Card").addEventListener('click', cardFlip);
+// let flipped = false;
+//
+//
+// function cardFlip () {
+//
+//     if (flipped == 'false') {
+//
+//         alert('not flipped')
+//
+//         document.querySelector('.Card').style.position = 'fixed';
+//         document.querySelector('.Card').style.zIndex = '1100';
+//         document.querySelector('.Card').style.left = '50%';
+//         document.querySelector('.Card').style.marginRight = '-50%';
+//         document.querySelector('.Card').style.transform = 'translate(-50%, -50%)';
+//         document.querySelector('.Card').style.top = '20vh';
+//         flipped = 'true';
+//
+//     }
+//
+//     if (flipped === 'true') {
+//
+//         alert('flipped')
+//
+//         document.querySelector('.Card').style.position = 'none';
+//         document.querySelector('.Card').style.zIndex = 'none';
+//         document.querySelector('.Card').style.left = 'none';
+//         document.querySelector('.Card').style.marginRight = 'none';
+//         document.querySelector('.Card').style.transform = 'none';
+//         document.querySelector('.Card').style.top = 'none';
+//
+//
+//         flipped = 'false'
+//     }
+//
+//
+// }
